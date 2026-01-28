99721bc8f599f68bde4deed1a7ac6d3c66755c9b
Wang-oss-tech <william.wang0817@gmail.com>
2026-01-28 12:02:19 -0500

    overlap is working

--- gemm/summa_manual_multicasting_pipelined_doubleColor/commands_wse2.sh
+++ gemm/summa_manual_multicasting_pipelined_doubleColor/commands_wse2.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-
-# Disable inst_trace for faster simulation
 # export SINGULARITYENV_SIMFABRIC_DEBUG=landing
 # export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
 
-
 cslc --arch=wse2 ./layout.csl --fabric-dims=11,6 --fabric-offsets=4,1 \
---params=P:4,Mt:28,Kt:28,Nt:28 \
+--params=P:4,Mt:14,Kt:14,Nt:14 \
 --memcpy --channels=1 -o out
 
 cs_python run.py --name out
