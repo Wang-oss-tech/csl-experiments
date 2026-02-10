f8346b2bf9001169f03e6705b234111d417cc73b
Wang-oss-tech <william.wang0817@gmail.com>
2026-02-09 23:59:47 -0500

    before system shutdown

--- gemm/summa_manual_multicasting/commands_wse2.sh
+++ gemm/summa_manual_multicasting/commands_wse2.sh
@@ -4,7 +4,7 @@ set -e
 
 # Disable inst_trace for faster simulation
 # export SINGULARITYENV_SIMFABRIC_DEBUG=landing
-# export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
+export SINGULARITYENV_SIMFABRIC_DEBUG=inst_trace
 
 
 cslc --arch=wse2 ./layout.csl --fabric-dims=11,6 --fabric-offsets=4,1 \
